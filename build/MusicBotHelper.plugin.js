cc19e8b28b108518907d388c8a8a5095823fe916
nataniel regula <nataniel.regula17@gmail.com>
2023-04-12 00:09:14 +0100

    added the ability to detect audio bot and using keybind to mute toggle the bot

---
+++ build/MusicBotHelper.plugin.js
@@ -0,0 +1,179 @@
+/**
+ * @name MusicBotHelper
+ * @description MusicBotHelper
+ * @version 0.0.1
+ * @author NR
+ * @source https://github.com/NatanielRegula/bd-voice-announcer
+ * @donate paypal.me/NatanielRegula
+ */
+/*@cc_on
+@if (@_jscript)
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
+
+@else@*/
+const config = {
+    main: "index.js",
+    id: "",
+    name: "MusicBotHelper",
+    author: "NR",
+    authorId: "",
+    authorLink: "",
+    version: "0.0.1",
+    description: "MusicBotHelper",
+    website: "",
+    source: "https://github.com/NatanielRegula/bd-voice-announcer",
+    patreon: "",
+    donate: "paypal.me/NatanielRegula",
+    invite: "",
+    changelog: [
+        {
+            title: "0.0.1",
+            type: "improved",
+            items: [
+                "initial"
+            ]
+        }
+    ]
+};
+class Dummy {
+    constructor() {this._config = config;}
+    start() {}
+    stop() {}
+}
+ 
+if (!global.ZeresPluginLibrary) {
+    BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.name ?? config.info.name} is missing. Please click Download Now to install it.`, {
+        confirmText: "Download Now",
+        cancelText: "Cancel",
+        onConfirm: () => {
+            require("request").get("https://betterdiscord.app/gh-redirect?id=9", async (err, resp, body) => {
+                if (err) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                if (resp.statusCode === 302) {
+                    require("request").get(resp.headers.location, async (error, response, content) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), content, r));
+                    });
+                }
+                else {
+                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                }
+            });
+        }
+    });
+}
+ 
+module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
+     const plugin = (Plugin, Library) => {
+  const { Logger, Utilities, WebpackModules, DiscordModules } = Library;
+
+  const Dispatcher = WebpackModules.getByProps('dispatch', 'subscribe');
+
+  const DisVoiceStateStore = WebpackModules.getByProps(
+    'getVoiceStateForUser',
+    'getVoiceStatesForChannel'
+  );
+
+  const DisUserStore = DiscordModules.UserStore;
+  const DisSelectedChannelStore = DiscordModules.SelectedChannelStore;
+  const DisAudioCtl = WebpackModules.getByProps(
+    'toggleLocalMute',
+    'setLocalVolume'
+  );
+  // BdApi.findModuleByProps('toggleLocalMute').toggleLocalMute("1062116752286298215")
+
+  return class MusicBotHelper extends Plugin {
+    constructor() {
+      super();
+      ///-----Adding Context To Methods-----///
+      //misc
+      this.getCurrentVoiceChannelUsersIds =
+        this.getCurrentVoiceChannelUsersIds.bind(this);
+      this.getIsUserABot = this.getIsUserABot.bind(this);
+      this.getCurrentlyActiveBotId = this.getCurrentlyActiveBotId.bind(this);
+
+      //audio actions
+      this.muteClientSide = this.muteClientSide.bind(this);
+
+      //misc
+      this.keyBindHandler = this.keyBindHandler.bind(this);
+    }
+
+    muteClientSide() {
+      const activeBotId = this.getCurrentlyActiveBotId();
+      DisAudioCtl.toggleLocalMute(activeBotId);
+    }
+
+    keyBindHandler(e) {
+      if (e.ctrlKey && e.altKey && e.code == 'KeyK') {
+        this.muteClientSide();
+      }
+    }
+
+    onStart() {
+      Logger.info('Plugin enabled!');
+
+      document.addEventListener('keydown', this.keyBindHandler);
+    }
+
+    onStop() {
+      Logger.info('Plugin disabled!');
+      document.removeEventListener('keydown', this.keyBindHandler);
+    }
+
+    ///-----Bot Detection-----///
+    getIsUserABot(userId) {
+      const userData = DisUserStore.getUser(userId);
+      return userData.bot;
+    }
+
+    getCurrentVoiceChannelUsersIds() {
+      const voiceStatesForCurrentVoiceChannelObject =
+        DisVoiceStateStore.getVoiceStatesForChannel(
+          DisSelectedChannelStore.getVoiceChannelId()
+        );
+
+      const currentVoiceChannelUsersIds = Object.keys(
+        voiceStatesForCurrentVoiceChannelObject
+      ).map((key) => voiceStatesForCurrentVoiceChannelObject[key].userId);
+
+      return currentVoiceChannelUsersIds;
+    }
+    getCurrentlyActiveBotId() {
+      //this will in the future allow to switch between multiple bots in vc
+      //for now it just gives the first form the list
+      return this.getMusicBotsInCurrentVoiceChat()[0];
+    }
+
+    getMusicBotsInCurrentVoiceChat() {
+      const currentVoiceChannelUsersIds = this.getCurrentVoiceChannelUsersIds();
+      const detectedBotsIds = [];
+      currentVoiceChannelUsersIds.forEach((userId) => {
+        if (this.getIsUserABot(userId)) {
+          detectedBotsIds.push(userId);
+        }
+      });
+      return detectedBotsIds;
+    }
+  };
+};
+     return plugin(Plugin, Api);
+})(global.ZeresPluginLibrary.buildPlugin(config));
+/*@end@*/
--- src/MusicBotHelper/index.js
+++ src/MusicBotHelper/index.js
@@ -11,17 +11,91 @@ module.exports = (Plugin, Library) => {
 
   const Dispatcher = WebpackModules.getByProps('dispatch', 'subscribe');
 
+  const DisVoiceStateStore = WebpackModules.getByProps(
+    'getVoiceStateForUser',
+    'getVoiceStatesForChannel'
+  );
+
+  const DisUserStore = DiscordModules.UserStore;
+  const DisSelectedChannelStore = DiscordModules.SelectedChannelStore;
+  const DisAudioCtl = WebpackModules.getByProps(
+    'toggleLocalMute',
+    'setLocalVolume'
+  );
+  // BdApi.findModuleByProps('toggleLocalMute').toggleLocalMute("1062116752286298215")
+
   return class MusicBotHelper extends Plugin {
     constructor() {
       super();
+      ///-----Adding Context To Methods-----///
+      //misc
+      this.getCurrentVoiceChannelUsersIds =
+        this.getCurrentVoiceChannelUsersIds.bind(this);
+      this.getIsUserABot = this.getIsUserABot.bind(this);
+      this.getCurrentlyActiveBotId = this.getCurrentlyActiveBotId.bind(this);
+
+      //audio actions
+      this.muteClientSide = this.muteClientSide.bind(this);
+
+      //misc
+      this.keyBindHandler = this.keyBindHandler.bind(this);
+    }
+
+    muteClientSide() {
+      const activeBotId = this.getCurrentlyActiveBotId();
+      DisAudioCtl.toggleLocalMute(activeBotId);
+    }
+
+    keyBindHandler(e) {
+      if (e.ctrlKey && e.altKey && e.code == 'KeyK') {
+        this.muteClientSide();
+      }
     }
 
     onStart() {
       Logger.info('Plugin enabled!');
+
+      document.addEventListener('keydown', this.keyBindHandler);
     }
 
     onStop() {
       Logger.info('Plugin disabled!');
+      document.removeEventListener('keydown', this.keyBindHandler);
+    }
+
+    ///-----Bot Detection-----///
+    getIsUserABot(userId) {
+      const userData = DisUserStore.getUser(userId);
+      return userData.bot;
+    }
+
+    getCurrentVoiceChannelUsersIds() {
+      const voiceStatesForCurrentVoiceChannelObject =
+        DisVoiceStateStore.getVoiceStatesForChannel(
+          DisSelectedChannelStore.getVoiceChannelId()
+        );
+
+      const currentVoiceChannelUsersIds = Object.keys(
+        voiceStatesForCurrentVoiceChannelObject
+      ).map((key) => voiceStatesForCurrentVoiceChannelObject[key].userId);
+
+      return currentVoiceChannelUsersIds;
+    }
+    getCurrentlyActiveBotId() {
+      //this will in the future allow to switch between multiple bots in vc
+      //for now it just gives the first form the list
+      return this.getMusicBotsInCurrentVoiceChat()[0];
+    }
+
+    getMusicBotsInCurrentVoiceChat() {
+      const currentVoiceChannelUsersIds = this.getCurrentVoiceChannelUsersIds();
+      const detectedBotsIds = [];
+      currentVoiceChannelUsersIds.forEach((userId) => {
+        if (this.getIsUserABot(userId)) {
+          detectedBotsIds.push(userId);
+        }
+      });
+      return detectedBotsIds;
     }
   };
 };
