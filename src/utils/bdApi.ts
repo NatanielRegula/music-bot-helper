a129766d40003a7580094f57af5adf9c307a5ade
Nataniel Regula <nataniel.regula17@gmail.com>
2023-09-17 17:06:00 +0100

    adds useState export with proper typing

--- src/utils/bdApi.ts
+++ src/utils/bdApi.ts
@@ -1,8 +1,9 @@
+import { useState as realUseState } from 'react';
 import config from '../../config.json';
-
 
 const BdApi = new window.BdApi(config.name);
 export const { UI, React, Patcher, Webpack, Data } = BdApi;
 
+export const useState: typeof realUseState = BdApi.React.useState;
 
 export default BdApi;
